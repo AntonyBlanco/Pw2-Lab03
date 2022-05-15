2e3605bee5c531fcf6dc650376800f5c729f8cda
Carlos Daniel Umasi Cariapaza <cumasic@unsa.edu.pe>
2022-05-15 17:17:52 -0500

    Cree un post que lee una carpeta de prueba y lo traduce de markdown a html

--- server.js
+++ server.js
@@ -11,3 +11,23 @@ app.listen(3000, () => {
 app.get('/', (request, response) => {
     response.sendFile(path.resolve(__dirname, 'index.html'));
 });
+app.get('/mostrar', (request, response) => {
+    response.sendFile(path.resolve(__dirname, 'form.html'));
+})
+app.post('/mostrar', (request, response) => {
+    fs.readFile(path.resolve(__dirname, 'pub/prueba.txt'), 'utf8',
+        (err, data) => {
+            if (err) {
+                console.error(err)
+                response.status(500).json({
+                    error: 'message'
+                })
+                return
+            }
+            let htmlText = md.render(data);
+            response.json({
+                text: htmlText.replace((/\n/g, '<br>'))
+            })
+        })
+
+});
