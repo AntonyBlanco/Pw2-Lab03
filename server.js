b0c7bbec453b9aba691e4157749991d899c525e6
AntonyBlanco <ablancot@unsa.edu.pe>
2022-05-14 23:53:04 -0500

    Servidor que lista los archivos de pub

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+const fs = require("fs");
 const path = require('path');
 const express = require('express');
+
 const app = express();
 
-app.use(express.static('pub'));
+const publicDir = "pub";
+
+app.use(express.static(publicDir));
 
 app.listen(3000, () => {
     console.log("Escuchando en el puerto 3000");
@@ -10,4 +14,22 @@ app.listen(3000, () => {
 
 app.get('/', (request, response) => {
     response.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
+// Listar archivos
+app.get('/listar', (request, response) => {
+	let objectSend = {};
+	let dirName = publicDir;
+	objectSend.dirName = dirName;
+	fs.readdir(publicDir, (err, files) => {
+		objectSend.fileArray = files;
+		response.send( objectSend );
+	});
+});
+app.get("/crear", (request, response) => {
+	let jsonContent = request.body;
+	jsonContent.text;
+});
+app.get("/mostrar", (req, res) => {
+	console.log("mostrando");
 });
