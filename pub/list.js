3af1eea9f219cba09790359cc4085d087d4287be
oliver092377 <omayta@unsa.edu.pe>
2022-05-16 21:23:17 -0500

    cambio a función fech

--- pub/list.js
+++ pub/list.js
@@ -1,3 +1,4 @@
+
 document.addEventListener("DOMContentLoaded", () => {
 	updateFileList();
 });
@@ -6,21 +7,40 @@ function updateFileList(){
 	let listDiv = document.querySelector("#listaArchivos");
 	let inner = "<ul>\n";
 	let jsonResponse;
+	
 	let xhttp = new XMLHttpRequest();
+	/*
 	xhttp.onreadystatechange = function(){
 		if (this.readyState == 4 && this.status == 200) {
 			jsonResponse = JSON.parse(this.responseText);
-			//console.log(jsonResponse);
+			console.log(jsonResponse);
 			for(file of jsonResponse.fileArray){
 				inner += "<li>" + file + "</li>\n";
 			}
 			inner += "</ul>\n";
-
 			listDiv.innerHTML = inner;
-
 		}
 	};
-	aaa = xhttp;
+	//aaa = xhttp;
 	xhttp.open("GET", "/listar");
 	xhttp.send();
+	*/
+	
+	
+	let url="/listar";
+
+	var promise=fetch(url);
+	promise.then(response => response.text())
+	.then(data=>{
+		jsonResponse = JSON.parse(data); 
+		console.log("tipe: "+typeof data);
+		console.log(jsonResponse);
+		for(file of jsonResponse.fileArray){
+			inner += "<li>" + file + "</li>\n";
+		}
+		inner += "</ul>\n";
+		listDiv.innerHTML = inner;
+	}).catch(error=>{
+		console.log("error"+error);
+	});
 }
