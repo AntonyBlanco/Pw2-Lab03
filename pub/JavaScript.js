db58087bc79f1f13b95cb3825b671097d672da97
oliver092377 <omayta@unsa.edu.pe>
2022-05-16 21:50:57 -0500

    cambio a funcion fetch

--- pub/JavaScript.js
+++ pub/JavaScript.js
@@ -14,22 +14,21 @@ function updateFileList(){
 	let listDiv = document.querySelector("#listaArchivos");
 	let inner = "<ul>\n";
 	let jsonResponse;
-	let xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function(){
-		if (this.readyState == 4 && this.status == 200) {
-			jsonResponse = JSON.parse(this.responseText);
-			//console.log(jsonResponse);
-			for(file of jsonResponse.fileArray){
-				inner += "<li><span onclick='mostrar(\""+file+"\")'>" + file + "</span></li>\n";
-			}
-			inner += "</ul>\n";
+	let url="/listar";
 
-			listDiv.innerHTML = inner;
-
+        var promise=fetch(url);
+        promise.then(response => response.text())
+        .then(data=>{
+                jsonResponse = JSON.parse(data);
+		//console.log(jsonResponse);
+		for(file of jsonResponse.fileArray){
+			inner += "<li><span onclick='mostrar(\""+file+"\")'>" + file + "</span></li>\n";
 		}
-	};
-	xhttp.open("GET", "/listar");
-	xhttp.send();
+		inner += "</ul>\n";
+		listDiv.innerHTML = inner;
+        }).catch(error=>{
+                console.log("error"+error);
+        });
 }
 
 function mostrar(file){
